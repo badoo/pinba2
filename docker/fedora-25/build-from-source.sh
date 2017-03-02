89d723d4632b327cee9033388df430cc565db41d
Anton Povarov <anton.povarov@gmail.com>
2017-03-02 14:50:38 +0300

    preliminary Dockerfile, based on fedora 25

---
+++ docker/fedora-25/build-from-source.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -xe
+
+# packages required for to build, can be safely removed when done
+DEV_PACKAGES="git-core gcc gcc-c++ boost-devel cmake autoconf automake libtool mariadb-devel"
+
+# packages required for operation
+PACKAGES="hostname mariadb-server"
+
+# install required packages
+# dnf install -y $DEV_PACKAGES
+# dnf install -y $PACKAGES
+
+# # create directories
+# mkdir -p /_src && cd /_src
+
+# # pull sources
+# git clone https://github.com/anton-povarov/pinba2
+# git clone https://github.com/anton-povarov/meow
+# git clone https://github.com/sparsehash/sparsehash
+# git clone https://github.com/nanomsg/nanomsg
+
+# build nanomsg and install (this one is a lil tricky to build statically)
+cd /_src/nanomsg
+cmake -DNN_STATIC_LIB=ON -DNN_ENABLE_DOC=OFF -DCMAKE_C_FLAGS="-fPIC -DPIC" -DCMAKE_INSTALL_PREFIX=/_install/nanomsg -DCMAKE_INSTALL_LIBDIR=lib .
+make -j4
+make install
+
+# sparsehash is simple to build (TODO: try disable building unit-tests as they take too much time)
+cd /_src/sparsehash
+./autogen.sh
+./configure --prefix=/_install/sparsehash
+make -j4
+make install
+
+# pinba
+cd /_src/pinba2
+./buildconf.sh
+./configure --prefix=/_install/pinba2 \
+	--with-mysql=/usr \
+	--with-boost=/usr \
+	--with-meow=/_src/meow \
+	--with-nanomsg=/_install/nanomsg \
+	--with-sparsehash=/_install/sparsehash
+make -j4
+
+# FIXME: this needs to be easier
+# for install, just copy stuff to mysql plugin dir
+cp /_src/pinba2/mysql_engine/.libs/libpinba_engine2.so `mysql_config --plugindir`
+
+# clean everything
+# rm -rf /_src
+# rm -rf /_install
+dnf remove -y $DEV_PACKAGES
+dnf autoremove -y
+# dnf clean all
---
+++ docker/fedora-25/docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -xe
+
+# this process is mildly copy-pasted from here
+# https://github.com/docker-library/mariadb/tree/b558f64b736650b94df9a90e68ff9e3bc03d4bdb/10.1
+
+if [ $1 = "mysqld" ]; then
+
+	# create default databases
+	# too expensive to perform on container startup really
+	mysql_install_db --rpm
+	chmod -R 777 /var/lib/mysql
+
+	# start mysql server in background for init process
+	"$@" --skip-networking --socket=/var/run/mysql/mysql.sock &
+	pid="$!"
+
+	# legen.... wait for it
+	for i in {30..0}; do
+		if echo 'SELECT 1' | mysql &> /dev/null; then
+			break
+		fi
+		echo 'MySQL init process in progress...'
+		sleep 1
+	done
+	if [ "$i" = 0 ]; then
+		echo >&2 'MySQL init process failed.'
+		exit 1
+	fi
+
+	# ..dary
+	# install plugin and create default db + tables
+	echo "INSTALL PLUGIN pinba SONAME 'libpinba_engine2.so';" | TERM=dumb mysql --protocol=socket -uroot
+	echo "CREATE DATABASE pinba;" | TERM=dumb mysql --protocol=socket -uroot
+
+
+	# terminate mysql server to start it in foreground
+	if ! kill -s TERM "$pid" || ! wait "$pid"; then
+		echo >&2 'MySQL init process failed.'
+		exit 1
+	fi
+fi
+
+exec "$@"
