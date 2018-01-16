badoo/pinba2

commit 413a9044eb71fc15020e164d281a57c1d23231f9

initial implementation of percent fields like old pinba

include/pinba/report.h
@@ -95,10 +95,17 @@ struct packet_t;
 struct dictionary_t;
 struct snapshot_dictionary_t;
 
+
 struct report_snapshot_t
 {
-	MEOW_DEFINE_SMART_ENUM_STRUCT(prepare_type,	((full,          "full"))
-												((no_histograms, "no_histograms")));
+	struct merge_flags
+	{
+		using type = uint32_t;
+		static constexpr type full          = 0;
+		static constexpr type no_histograms = (1 << 0);
+		static constexpr type no_totals     = (1 << 1);
+	};
+	using merge_flags_t = merge_flags::type;
 
 	// this struct is just a placeholder, same size as real hashtable iterator
 	// FIXME: what about alignment here?
@@ -127,7 +134,7 @@ struct report_snapshot_t
 	// MUST be called before any of the functions below
 	// this exists primarily to allow preparation to take place in a thread
 	// different from the one handling report data (more parallelism, yey)
-	virtual void prepare(prepare_type_t = prepare_type::full) = 0;
+	virtual void prepare(merge_flags_t flags = 0) = 0;
 	virtual bool is_prepared() const = 0;
 
 	// will return 0 if !is_prepared()
@@ -149,6 +156,7 @@ struct report_snapshot_t
 	// data handling
 	virtual int   data_kind() const = 0;
 	virtual void* get_data(position_t const&) = 0;
+	virtual void* get_data_totals() const = 0;
 
 	// histograms
 	virtual int   histogram_kind() const = 0;

include/pinba/report_util.h
@@ -157,13 +157,20 @@ struct report_snapshot_traits___example
 {
 	using src_ticks_t = ; // source ticks_ringbuffer_t
 	using hashtable_t = ; // result hashtable (that we're going to iterate over)
+	using totals_t    = ; // struct, holding overall report data totals
 
 	// merge ticks from src_ticks_t to hashtable_t
 	static void merge_ticks_into_data(
 			  report_snapshot_ctx_t *snapshot_ctx
 			, src_ticks_t& ticks
 			, hashtable_t& to
-			, report_snapshot_t::prepare_type_t ptype);
+			, report_snapshot_t::merge_flags_t flags);
+
+	// calculate totals over the final report
+	static void calculate_totals(
+		  report_snapshot_ctx_t *snapshot_ctx
+		, totals_t *totals
+		, hashtable_t const& data);
 
 	// get iterator keys/values/histograms at iterator
 	static report_key_t key_at_position(hashtable_t const&, iterator_t const&);
@@ -202,11 +209,13 @@ struct report_snapshot__impl_t
 	using src_ticks_t = typename Traits::src_ticks_t;
 	using hashtable_t = typename Traits::hashtable_t;
 	using iterator_t  = typename hashtable_t::iterator;
+	using totals_t    = typename Traits::totals_t;
 
 public: // intentional, internal use only
 
 	hashtable_t  data_;      // real data we iterate over
 	src_ticks_t  ticks_;     // ticks we merge our data from (in other thread potentially)
+	totals_t     totals_;    // totals storage
 	bool         prepared_;  // has data been prepared?
 
 public:
@@ -230,19 +239,22 @@ private:
 		return globals->dictionary();
 	}
 
-	virtual void prepare(prepare_type_t ptype) override
+	virtual void prepare(merge_flags_t flags) override
 	{
 		if (this->is_prepared())
 			return;
 
 		meow::stopwatch_t sw;
 
-		Traits::merge_ticks_into_data(this, ticks_, data_, ptype);
+		Traits::merge_ticks_into_data(this, ticks_, data_, flags);
 
 		prepared_ = true;
 
 		if (this->stats)
 			this->stats->last_snapshot_merge_d = duration_from_timeval(sw.stamp());
+
+		if ((merge_flags::no_totals & flags) == 0)
+			Traits::calculate_totals(this, &totals_, data_);
 
 		// do NOT clear ticks here, as snapshot impl might want to keep ref to it
 		// ticks_.clear();
@@ -343,6 +355,11 @@ private:
 	{
 		auto const& it = iterator_from_position(pos);
 		return Traits::value_at_position(data_, it);
+	}
+
+	virtual void* get_data_totals() const override
+	{
+		return (void*)&totals_;
 	}
 
 	virtual int histogram_kind() const override

mysql_engine/handler.cpp
@@ -36,6 +36,32 @@
 	break;
 /**/
 
+#define STORE_PERCENT_I(N, row_value, total_value)                 \
+	case N: {                                                      \
+		if ((total_value) == 0) { (*field)->set_null(); }          \
+		else {                                                     \
+			auto const val = (double)(row_value) / (total_value);  \
+			(*field)->set_notnull();                               \
+			(*field)->store(val * 100.0);                          \
+		}                                                          \
+	}; break;                                                      \
+/**/
+
+#define STORE_PERCENT_D(N, row_value, total_value)                 \
+	case N: {                                                      \
+		if ((total_value).nsec == 0) { (*field)->set_null(); }     \
+		else {                                                     \
+			auto const val =                                       \
+				duration_seconds_as_double(row_value)              \
+				/ duration_seconds_as_double(total_value);         \
+			(*field)->set_notnull();                               \
+			(*field)->store(val * 100.0);                          \
+		}                                                          \
+	}; break;                                                      \
+/**/
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
 struct pinba_view___base_t : public pinba_view_t
 {
 	pinba_view___base_t()
@@ -690,14 +716,15 @@ private:
 		{
 			meow::stopwatch_t sw;
 
-			auto const ptype = (need_percentiles)
-								? report_snapshot_t::prepare_type::full
-								: report_snapshot_t::prepare_type::no_histograms;
-			snapshot_->prepare(ptype);
+			auto const flags = (need_percentiles)
+								? report_snapshot_t::merge_flags::full
+								: report_snapshot_t::merge_flags::no_histograms;
 
-			LOG_DEBUG(P_L_, "snapshot::{0}; snapshot for: {1}, prepare ({2}) took {3} seconds ({4} rows)",
+			snapshot_->prepare(flags);
+
+			LOG_DEBUG(P_L_, "snapshot::{0}; snapshot for: {1}, prepare (flags: {2}) took {3} seconds ({4} rows)",
 				__func__, share_data_->mysql_name,
-				report_snapshot_t::prepare_type::enum_as_str_ref(ptype),
+				ff::as_hex(flags),
 				sw.stamp(), snapshot_->row_count());
 		}
 
@@ -758,21 +785,40 @@ private:
 				constexpr unsigned const n_data_fields = n_data_fields___by_request;
 				if (findex < n_data_fields)
 				{
-					auto const *row = reinterpret_cast<report_row_data___by_request_t*>(snapshot_->get_data(row_pos));
+					auto const *row    = reinterpret_cast<report_row_data___by_request_t*>(snapshot_->get_data(row_pos));
+					auto const *totals = reinterpret_cast<report_row_data___by_request_t*>(snapshot_->get_data_totals());
 
 					switch (findex)
 					{
-						STORE_FIELD(0,  row->req_count);
-						STORE_FIELD(1,  double(row->req_count) / duration_seconds_as_double(rinfo->time_window));
-						STORE_FIELD(2,  duration_seconds_as_double(row->time_total));
-						STORE_FIELD(3,  duration_seconds_as_double(row->time_total) / duration_seconds_as_double(rinfo->time_window));
-						STORE_FIELD(4,  duration_seconds_as_double(row->ru_utime));
-						STORE_FIELD(5,  duration_seconds_as_double(row->ru_utime) / duration_seconds_as_double(rinfo->time_window));
-						STORE_FIELD(6,  duration_seconds_as_double(row->ru_stime));
-						STORE_FIELD(7,  duration_seconds_as_double(row->ru_stime) / duration_seconds_as_double(rinfo->time_window));
-						STORE_FIELD(8,  row->traffic);
-						STORE_FIELD(9,  double(row->traffic) / duration_seconds_as_double(rinfo->time_window));
-						STORE_FIELD(10, row->mem_used);
+						// req_count
+						STORE_FIELD    (0,  row->req_count);
+						STORE_FIELD    (1,  double(row->req_count) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_I(2,  row->req_count, totals->req_count);
+
+						// time_total
+						STORE_FIELD    (3,  duration_seconds_as_double(row->time_total));
+						STORE_FIELD    (4,  duration_seconds_as_double(row->time_total) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_D(5,  row->time_total, totals->time_total);
+
+						// ru_utime
+						STORE_FIELD    (6,  duration_seconds_as_double(row->ru_utime));
+						STORE_FIELD    (7,  duration_seconds_as_double(row->ru_utime) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_D(8,  row->ru_utime, totals->ru_utime);
+
+						// ru_stime
+						STORE_FIELD    (9,  duration_seconds_as_double(row->ru_stime));
+						STORE_FIELD    (10, duration_seconds_as_double(row->ru_stime) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_D(11, row->ru_stime, totals->ru_stime);
+
+						// traffic
+						STORE_FIELD    (12, row->traffic);
+						STORE_FIELD    (13, double(row->traffic) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_I(14, row->traffic, totals->traffic);
+
+						// mem_used
+						STORE_FIELD    (15, row->mem_used);
+						STORE_FIELD    (16, double(row->mem_used) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_I(17, row->mem_used, totals->mem_used);
 					}
 
 					continue;
@@ -784,20 +830,35 @@ private:
 				static unsigned const n_data_fields = n_data_fields___by_timer;
 				if (findex < n_data_fields)
 				{
-					auto const *row = reinterpret_cast<report_row_data___by_timer_t*>(snapshot_->get_data(row_pos));
+					auto const *row    = reinterpret_cast<report_row_data___by_timer_t*>(snapshot_->get_data(row_pos));
+					auto const *totals = reinterpret_cast<report_row_data___by_timer_t*>(snapshot_->get_data_totals());
 
 					switch (findex)
 					{
-						STORE_FIELD(0, row->req_count);
-						STORE_FIELD(1, double(row->req_count) / duration_seconds_as_double(rinfo->time_window));
-						STORE_FIELD(2, row->hit_count);
-						STORE_FIELD(3, double(row->hit_count) / duration_seconds_as_double(rinfo->time_window));
-						STORE_FIELD(4, duration_seconds_as_double(row->time_total));
-						STORE_FIELD(5, duration_seconds_as_double(row->time_total) / duration_seconds_as_double(rinfo->time_window));
-						STORE_FIELD(6, duration_seconds_as_double(row->ru_utime));
-						STORE_FIELD(7, duration_seconds_as_double(row->ru_utime) / duration_seconds_as_double(rinfo->time_window));
-						STORE_FIELD(8, duration_seconds_as_double(row->ru_stime));
-						STORE_FIELD(9, duration_seconds_as_double(row->ru_stime) / duration_seconds_as_double(rinfo->time_window));
+						// req_count
+						STORE_FIELD    (0, row->req_count);
+						STORE_FIELD    (1, double(row->req_count) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_I(2, row->req_count, totals->req_count);
+
+						// hit_count
+						STORE_FIELD    (3, row->hit_count);
+						STORE_FIELD    (4, double(row->hit_count) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_I(5, row->hit_count, totals->hit_count);
+
+						// time_total
+						STORE_FIELD    (6, duration_seconds_as_double(row->time_total));
+						STORE_FIELD    (7, duration_seconds_as_double(row->time_total) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_D(8, row->time_total, totals->time_total);
+
+						// ru_utime
+						STORE_FIELD    (9, duration_seconds_as_double(row->ru_utime));
+						STORE_FIELD    (10, duration_seconds_as_double(row->ru_utime) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_D(11, row->ru_utime, totals->ru_utime);
+
+						// ru_stime
+						STORE_FIELD    (12, duration_seconds_as_double(row->ru_stime));
+						STORE_FIELD    (13, duration_seconds_as_double(row->ru_stime) / duration_seconds_as_double(rinfo->time_window));
+						STORE_PERCENT_D(14, row->ru_stime, totals->ru_stime);
 					}
 
 					continue;

src/report_by_packet.cpp
@@ -165,6 +165,7 @@ namespace { namespace aux {
 		{
 			struct snapshot_traits
 			{
+				using totals_t    = report_row_data___by_packet_t;
 				using src_ticks_t = ringbuffer_t;
 				using hashtable_t = std::array<report_row___by_packet_t, 1>; // array to get iterators 'for free'
 
@@ -172,13 +173,28 @@ namespace { namespace aux {
 				static void*        value_at_position(hashtable_t const&, hashtable_t::iterator const& it)  { return (void*)it; }
 				static void*        hv_at_position(hashtable_t const&, hashtable_t::iterator const& it)     { return &it->hv; }
 
+				static void calculate_totals(report_snapshot_ctx_t *snapshot_ctx, totals_t *totals, hashtable_t const& data)
+				{
+					auto const& row = data[0].data;
+
+					totals->req_count   += row.req_count;
+					totals->timer_count += row.timer_count;
+					totals->time_total  += row.time_total;
+					totals->ru_utime    += row.ru_utime;
+					totals->ru_stime    += row.ru_stime;
+					totals->traffic     += row.traffic;
+					totals->mem_used    += row.mem_used;
+				}
+
 				// merge from src ringbuffer to snapshot data
 				static void merge_ticks_into_data(
 					  report_snapshot_ctx_t *snapshot_ctx
 					, src_ticks_t& ticks
 					, hashtable_t& to
-					, report_snapshot_t::prepare_type_t ptype)
+					, report_snapshot_t::merge_flags_t flags)
 				{
+					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && (flags & report_snapshot_t::merge_flags::no_histograms));
+
 					MEOW_DEFER(
 						ticks.clear();
 					);
@@ -199,7 +215,7 @@ namespace { namespace aux {
 						dst.data.traffic     += src.data.traffic;
 						dst.data.mem_used    += src.data.mem_used;
 
-						if (snapshot_ctx->rinfo.hv_enabled)
+						if (need_histograms)
 						{
 							dst.hv.merge_other(src.hv);
 						}

src/report_by_request.cpp
@@ -220,6 +220,7 @@ namespace { namespace aux {
 			struct snapshot_traits
 			{
 				using src_ticks_t = ringbuffer_t;
+				using totals_t    = report_row_data___by_request_t;
 
 				struct row_t
 				{
@@ -233,6 +234,8 @@ namespace { namespace aux {
 				{
 					hashtable_t() { this->set_empty_key(report_key_impl___make_empty<NKeys>()); }
 				};
+
+			public:
 
 				static report_key_t key_at_position(hashtable_t const&, typename hashtable_t::iterator const& it)
 				{
@@ -249,14 +252,29 @@ namespace { namespace aux {
 					return &it->second.hv;
 				}
 
+				static void calculate_totals(report_snapshot_ctx_t *snapshot_ctx, totals_t *totals, hashtable_t const& data)
+				{
+					for (auto const& data_pair : data)
+					{
+						auto const& row = data_pair.second.data;
+
+						totals->req_count  += row.req_count;
+						totals->time_total += row.time_total;
+						totals->ru_utime   += row.ru_utime;
+						totals->ru_stime   += row.ru_stime;
+						totals->traffic    += row.traffic;
+						totals->mem_used   += row.mem_used;
+					}
+				}
+
 				// merge from src ringbuffer to snapshot data
 				static void merge_ticks_into_data(
 					  report_snapshot_ctx_t *snapshot_ctx
 					, src_ticks_t& ticks
 					, hashtable_t& to
-					, report_snapshot_t::prepare_type_t ptype)
+					, report_snapshot_t::merge_flags_t flags)
 				{
-					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && ptype != report_snapshot_t::prepare_type::no_histograms);
+					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && (flags & report_snapshot_t::merge_flags::no_histograms));
 
 					uint64_t key_lookups = 0;
 					uint64_t hv_lookups = 0;

src/report_by_timer.cpp
@@ -573,6 +573,7 @@ namespace { namespace aux {
 			struct snapshot_traits
 			{
 				using src_ticks_t = ringbuffer_t;
+				using totals_t    = report_row_data___by_timer_t;
 
 				struct row_t
 				{
@@ -673,14 +674,28 @@ namespace { namespace aux {
 					return &row->merged_hv;
 				}
 
+				static void calculate_totals(report_snapshot_ctx_t *snapshot_ctx, totals_t *totals, hashtable_t const& data)
+				{
+					for (auto const& data_pair : data)
+					{
+						auto const& row = data_pair.second.data;
+
+						totals->req_count  += row.req_count;
+						totals->hit_count  += row.hit_count;
+						totals->time_total += row.time_total;
+						totals->ru_utime   += row.ru_utime;
+						totals->ru_stime   += row.ru_stime;
+					}
+				}
+
 				// merge from src ringbuffer to snapshot data
 				static void merge_ticks_into_data(
 					  report_snapshot_ctx_t *snapshot_ctx
 					, src_ticks_t& ticks
 					, hashtable_t& to
-					, report_snapshot_t::prepare_type_t ptype)
+					, report_snapshot_t::merge_flags_t flags)
 				{
-					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && ptype != report_snapshot_t::prepare_type::no_histograms);
+					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && (flags & report_snapshot_t::merge_flags::no_histograms));
 
 					uint64_t n_ticks = 0;
 					uint64_t key_lookups = 0;

experiments/exp_aggregation.cpp
@@ -323,17 +323,31 @@ public:
 		{
 			using src_ticks_t = ringbuffer_t;
 			using hashtable_t = std::array<report_row___by_packet_t, 1>; // array to get iterators 'for free'
+			using totals_t    = report_row_data___by_packet_t;
 
 			static report_key_t key_at_position(hashtable_t const&, hashtable_t::iterator const& it)    { return {}; }
 			static void*        value_at_position(hashtable_t const&, hashtable_t::iterator const& it)  { return (void*)it; }
 			static void*        hv_at_position(hashtable_t const&, hashtable_t::iterator const& it)     { return &it->hv; }
+
+			static void calculate_totals(report_snapshot_ctx_t *snapshot_ctx, totals_t *totals, hashtable_t const& data)
+			{
+				auto const& row = data[0].data;
+
+				totals->req_count   += row.req_count;
+				totals->timer_count += row.timer_count;
+				totals->time_total  += row.time_total;
+				totals->ru_utime    += row.ru_utime;
+				totals->ru_stime    += row.ru_stime;
+				totals->traffic     += row.traffic;
+				totals->mem_used    += row.mem_used;
+			}
 
 			// merge from src ringbuffer to snapshot data
 			static void merge_ticks_into_data(
 				  report_snapshot_ctx_t *snapshot_ctx
 				, src_ticks_t& ticks
 				, hashtable_t& to
-				, report_snapshot_t::prepare_type_t ptype)
+				, report_snapshot_t::merge_flags_t flags)
 			{
 				MEOW_DEFER(
 					ticks.clear();
@@ -439,17 +453,22 @@ public:
 		{
 			using src_ticks_t = ringbuffer_t;
 			using hashtable_t = std::array<report_row___by_packet_t, 1>; // array to get iterators 'for free'
+			using totals_t    = report_row_data___by_packet_t;
 
 			static report_key_t key_at_position(hashtable_t const&, hashtable_t::iterator const& it)    { return {}; }
 			static void*        value_at_position(hashtable_t const&, hashtable_t::iterator const& it)  { return (void*)it; }
 			static void*        hv_at_position(hashtable_t const&, hashtable_t::iterator const& it)     { return &it->hv; }
+
+			static void calculate_totals(report_snapshot_ctx_t *snapshot_ctx, totals_t *totals, hashtable_t const& data)
+			{
+			}
 
 			// merge from src ringbuffer to snapshot data
 			static void merge_ticks_into_data(
 				  report_snapshot_ctx_t *snapshot_ctx
 				, src_ticks_t& ticks
 				, hashtable_t& to
-				, report_snapshot_t::prepare_type_t ptype)
+				, report_snapshot_t::merge_flags_t flags)
 			{
 			}
 		};

scripts/default_reports.sql
@@ -11,43 +11,64 @@ CREATE TABLE `report_by_host_name` (
   `host` varchar(64) NOT NULL,
   `req_count` int(10) unsigned NOT NULL,
   `req_per_sec` float NOT NULL,
+  `req_percent` float,
   `req_time_total` float NOT NULL,
   `req_time_per_sec` float NOT NULL,
+  `req_time_percent` float,
   `ru_utime_total` float NOT NULL,
   `ru_utime_per_sec` float NOT NULL,
+  `ru_utime_percent` float,
   `ru_stime_total` float NOT NULL,
   `ru_stime_per_sec` float NOT NULL,
+  `ru_stime_percent` float,
   `traffic_total` bigint(20) unsigned NOT NULL,
   `traffic_per_sec` float NOT NULL,
-  `memory_footprint` bigint(20) NOT NULL
+  `traffic_percent` float,
+  `memory_footprint` bigint(20) NOT NULL,
+  `memory_per_sec` float NOT NULL,
+  `memory_percent` float
 ) ENGINE=PINBA DEFAULT CHARSET=latin1 COMMENT='v2/request/default_history_time/~host/no_percentiles/no_filters';
 
 CREATE TABLE `report_by_script_name` (
   `script` varchar(64) NOT NULL,
   `req_count` int(10) unsigned NOT NULL,
   `req_per_sec` float NOT NULL,
+  `req_percent` float,
   `req_time_total` float NOT NULL,
   `req_time_per_sec` float NOT NULL,
+  `req_time_percent` float,
   `ru_utime_total` float NOT NULL,
   `ru_utime_per_sec` float NOT NULL,
+  `ru_utime_percent` float,
   `ru_stime_total` float NOT NULL,
   `ru_stime_per_sec` float NOT NULL,
+  `ru_stime_percent` float,
   `traffic_total` bigint(20) unsigned NOT NULL,
   `traffic_per_sec` float NOT NULL,
-  `memory_footprint` bigint(20) NOT NULL
+  `traffic_percent` float,
+  `memory_footprint` bigint(20) NOT NULL,
+  `memory_per_sec` float NOT NULL,
+  `memory_percent` float
 ) ENGINE=PINBA DEFAULT CHARSET=latin1 COMMENT='v2/request/default_history_time/~script/no_percentiles/no_filters';
 
 CREATE TABLE `report_by_server_name` (
   `server` varchar(64) NOT NULL,
   `req_count` int(10) unsigned NOT NULL,
   `req_per_sec` float NOT NULL,
+  `req_percent` float,
   `req_time_total` float NOT NULL,
   `req_time_per_sec` float NOT NULL,
+  `req_time_percent` float,
   `ru_utime_total` float NOT NULL,
   `ru_utime_per_sec` float NOT NULL,
+  `ru_utime_percent` float,
   `ru_stime_total` float NOT NULL,
   `ru_stime_per_sec` float NOT NULL,
+  `ru_stime_percent` float,
   `traffic_total` bigint(20) unsigned NOT NULL,
   `traffic_per_sec` float NOT NULL,
-  `memory_footprint` bigint(20) NOT NULL
+  `traffic_percent` float,
+  `memory_footprint` bigint(20) NOT NULL,
+  `memory_per_sec` float NOT NULL,
+  `memory_percent` float
 ) ENGINE=PINBA DEFAULT CHARSET=latin1 COMMENT='v2/request/default_history_time/~server/no_percentiles/no_filters';

commit 445c3a708601c0690a3a8afb89712fee1bd59b69

fixed percentile fields reporting (broken since 413a9044eb71fc15020e164d281a57c1d23231f9)

include/pinba/report.h
@@ -101,9 +101,9 @@ struct report_snapshot_t
 	struct merge_flags
 	{
 		using type = uint32_t;
-		static constexpr type full          = 0;
-		static constexpr type no_histograms = (1 << 0);
-		static constexpr type no_totals     = (1 << 1);
+		static constexpr type none            = 0;
+		static constexpr type with_histograms = (1 << 0);
+		static constexpr type with_totals     = (1 << 1);
 	};
 	using merge_flags_t = merge_flags::type;
 

include/pinba/report_util.h
@@ -253,7 +253,7 @@ private:
 		if (this->stats)
 			this->stats->last_snapshot_merge_d = duration_from_timeval(sw.stamp());
 
-		if ((merge_flags::no_totals & flags) == 0)
+		if (flags & merge_flags::with_totals)
 			Traits::calculate_totals(this, &totals_, data_);
 
 		// do NOT clear ticks here, as snapshot impl might want to keep ref to it

mysql_engine/handler.cpp
@@ -497,7 +497,7 @@ struct pinba_view___report_snapshot_t : public pinba_view___base_t
 	report_snapshot_t::position_t  curr_pos_;   // last returned row pos, for position()
 
 	static constexpr unsigned const n_data_fields___by_request = 18;
-	static constexpr unsigned const n_data_fields___by_timer   = 14;
+	static constexpr unsigned const n_data_fields___by_timer   = 15;
 	static constexpr unsigned const n_data_fields___by_packet  = 7;
 
 public:
@@ -716,9 +716,14 @@ private:
 		{
 			meow::stopwatch_t sw;
 
-			auto const flags = (need_percentiles)
-								? report_snapshot_t::merge_flags::full
-								: report_snapshot_t::merge_flags::no_histograms;
+			report_snapshot_t::merge_flags_t flags = 0;
+
+			// always want totals, for percent fields
+			flags |= report_snapshot_t::merge_flags::with_totals;
+
+			// maybe want histograms if percentile fields are being selected
+			if (need_percentiles)
+				flags |= report_snapshot_t::merge_flags::with_histograms;
 
 			snapshot_->prepare(flags);
 

src/report_by_packet.cpp
@@ -193,7 +193,7 @@ namespace { namespace aux {
 					, hashtable_t& to
 					, report_snapshot_t::merge_flags_t flags)
 				{
-					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && (flags & report_snapshot_t::merge_flags::no_histograms));
+					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && (flags & report_snapshot_t::merge_flags::with_histograms));
 
 					MEOW_DEFER(
 						ticks.clear();

src/report_by_request.cpp
@@ -274,7 +274,7 @@ namespace { namespace aux {
 					, hashtable_t& to
 					, report_snapshot_t::merge_flags_t flags)
 				{
-					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && (flags & report_snapshot_t::merge_flags::no_histograms));
+					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && (flags & report_snapshot_t::merge_flags::with_histograms));
 
 					uint64_t key_lookups = 0;
 					uint64_t hv_lookups = 0;

src/report_by_timer.cpp
@@ -695,7 +695,7 @@ namespace { namespace aux {
 					, hashtable_t& to
 					, report_snapshot_t::merge_flags_t flags)
 				{
-					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && (flags & report_snapshot_t::merge_flags::no_histograms));
+					bool const need_histograms = (snapshot_ctx->rinfo.hv_enabled && (flags & report_snapshot_t::merge_flags::with_histograms));
 
 					uint64_t n_ticks = 0;
 					uint64_t key_lookups = 0;