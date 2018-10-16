6a218d2e4d6a092efed66ba8bf8715bd345b6ed1
Alexander Ilyin <alexander@ilyin.eu>
2018-10-15 22:31:05 -0700

    Cleanup;

--- docker/build-from-source.sh
+++ docker/build-from-source.sh
@@ -1,23 +1,5 @@
 #!/bin/bash -xe
 
-# packages required for to build, can be safely removed when done
-DEV_PACKAGES="git-core gcc gcc-c++ boost-devel cmake autoconf automake libtool mariadb-devel"
-
-# packages required for operation
-PACKAGES="hostname mariadb-server"
-
-# install required packages
-# dnf install -y $DEV_PACKAGES
-# dnf install -y $PACKAGES
-
-# # create directories
-# mkdir -p /_src && cd /_src
-
-# # pull sources
-# git clone https://github.com/anton-povarov/pinba2
-# git clone https://github.com/anton-povarov/meow
-# git clone https://github.com/nanomsg/nanomsg
-
 # build nanomsg and install (this one is a lil tricky to build statically)
 cd /_src/nanomsg
 cmake \
@@ -45,7 +27,3 @@ make -j4
 # FIXME: this needs to be easier
 # for install, just copy stuff to mysql plugin dir
 cp /_src/pinba2/mysql_engine/.libs/libpinba_engine2.so `mysql_config --plugindir`
-
-# clean everything
-rm -rf /_src
-rm -rf /_install
