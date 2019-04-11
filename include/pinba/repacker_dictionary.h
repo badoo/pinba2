78315849e48befec8bdb0842fd6fdbd3445b587d
Anton Povarov <antoxa@corp.badoo.com>
2019-04-11 15:09:22 +0300

    repacker_dictionary: reworked wordslice hastable from map<id, word_ptr> to set<word_ptr>

--- include/pinba/repacker_dictionary.h
+++ include/pinba/repacker_dictionary.h
@@ -7,6 +7,7 @@
 
 #include <t1ha/t1ha.h>
 #include <tsl/robin_map.h>
+#include <tsl/robin_set.h>
 
 #include <meow/intrusive_ptr.hpp>
 
@@ -42,26 +43,31 @@ struct repacker_dictionary_t : private boost::noncopyable
 	};
 	using word_ptr = boost::intrusive_ptr<word_t>;
 
+	// a list of words, referencing this dictionary, that can be given away
+	// to reports and/or selects to keep those words from being removed from the dictionary
+	// while report or select is still using them
+	//
+	// words are ogranized into slices, to allow for bulk removal in a time-series manner
+	//
+	// internal hashtable stores pointers to word objects (see below) word_to_id_hash_t
+	// the thing here is to make hash lookups never reference anything outside the table itself
+	//  (prev implementation stored pairs or <word_ptr->id, word_ptr>, costing us cache misses)
+	// the hash function is subject to change, but would very much like to keep it close to 'identity'
+	//  since word pointers are pretty well distributed, but might be 'too regular', with arena allocators
 	struct wordslice_t : public meow::ref_counted_t
 	{
-		struct word_id_hasher_t
+		struct hash_function_t
 		{
-			inline uint64_t operator()(uint32_t const key) const
+			inline uint64_t operator()(word_ptr const& wp) const
 			{
-				return t1ha0(&key, sizeof(key), key);
+				uint64_t const as_uint = *reinterpret_cast<uint64_t*>(wp.get());
+				return (as_uint >> 3); // word is at least 8-byte aligned, so those bits are useless zeroes
 			}
 		};
 
-		struct hash_t : public tsl::robin_map<
-											  uint32_t
-											, word_ptr
-											, word_id_hasher_t // TODO: try identity hash function here, should be ok
-											, std::equal_to<uint32_t>
-											, std::allocator<std::pair<uint32_t, word_ptr>>
-											, /*StoreHash=*/ true>
+		struct hash_t : public tsl::robin_set<word_ptr, hash_function_t>
 		{
 		};
-
 		hash_t ht;
 
 		wordslice_t()
@@ -76,7 +82,11 @@ struct repacker_dictionary_t : private boost::noncopyable
 	};
 	using wordslice_ptr = boost::intrusive_ptr<wordslice_t>;
 
-	// hashtable should support efficient deletion
+	// a cache str_ref -> word_ptr
+	// word_ptr is a local object, does not reference global dictionary
+	//  but references the (immutable) word string, stored in global dictionary
+	//
+	// this hashtable should support efficient deletion
 	// but dense_hash_map degrades ridiculously under high number of erases
 	// so standard chaining-based unoredered_map is used
 	struct word_to_id_hash_t : public tsl::robin_map<
@@ -125,6 +135,14 @@ public:
 			this->add_to_current_wordslice(it->second.get());
 			return it->second->id;
 		}
+
+		// TODO(antoxa): actually implement and check performance benefit
+		// XXX(antoxa): ugly hack, to avoid extra hash lookup
+		//  the point here, is that we can't insert with `word` as key,
+		//  since `word` might reference temporary memory but we do it anyway,
+		//  and later, if insert was actually successful - we're going to replace the key,
+		//  so that it references the equivalent string from actual upstream dictionary word (with proper lifetime)
+		// auto inserted_pair = word_to_id.emplace_hash(word_hash, word, word_ptr{nullptr});
 
 		// cache miss - slowpath
 		word_ptr w = [&]() {
@@ -197,36 +215,47 @@ public:
 				result.reaped_slices      += 1;
 				result.reaped_words_local += ws->ht.size();
 
-				// reduce refcounts to all words in this slice
-				for(auto ht_it = ws->ht.begin(); ht_it != ws->ht.end(); ++ht_it)
+				// check refcounts to all words in this slice
+				// and maybe delete from local dictionary if only this wordslice and local dictionary reference the word
+				// this class is single-threaded, so noone is modifying anything while we're on it
+				for (auto const& w : ws->ht)
 				{
-					// this class is single-threaded, so noone is modifying refcount while we're on it
-					word_ptr& w = ht_it.value();
 					assert(w->use_count() >= 2); // at least `this->word_to_id` and `w` must reference the word
 
 					// LOG_DEBUG(PINBA_LOOGGER_, "{0}; '{1}' {2} {3}", __func__, w->str, w->id, w->use_count());
 
-					if (w->use_count() == 2)     // can erase, since ONLY `this->word_to_id` and `w` reference the word
+					if (w->use_count() == 2) // can erase, since ONLY `this->word_to_id` and `w` reference the word
 					{
-						result.reaped_words_global += 1;
-
 						// LOG_DEBUG(PINBA_LOOGGER_, "reap_unused_wordslices; erase global '{0}' {1} {2}", w->str, w->id, w->use_count());
 
 						// erase from local hash
 						size_t const erased_count = word_to_id.erase(w->str, w->hash);
 						assert(erased_count == 1);
 
-						// try moving, to avoid jerking refcount back-n-forth
+						// save the word for later erase_ref from upstream dictionary
+						// interusive_ptr doesn't support moves and `w` is a 'const&' anyway
 						assert(w->use_count() == 1);
-						words_to_erase.emplace_back(std::move(w));
+						words_to_erase.emplace_back(w);
 					}
 					else
 					{
-						word_t *wptr = w.get();          // save
-						w.reset();                       // just deref as usual, if no delete is needed
-						assert(wptr->use_count() >= 2);  // some other wordslice must still be holding a ref
+						// now, this is ugly, but at this point
+						// memory where `w` points to is in L1 cache, so do decref immediately
+						// this action also leaves nullptr in place of previous address here
+						// so `ws` dtor becomes a table scan, but a noop otherwise
+						word_ptr& w_nonconst = const_cast<word_ptr&>(w);
+
+						word_t *wptr = w_nonconst.get();  // save for later assert
+						w_nonconst.reset();               // just decref, leave nullptr behind (this breaks hashtable invariant btw)
+						assert(wptr->use_count() >= 2);   // some other wordslice must still be holding a ref
 					}
 				}
+
+				// destroy wordslice here and don't wait for erase to do it
+				// this is important to decrement all refcounts early
+				// as next wordslice might contain the same word
+				//  and the above w->use_count() logic relies on refcounts being accurate
+				// ws.reset();
 			}
 
 			return words_to_erase;
@@ -247,6 +276,8 @@ public:
 			d->erase_word___ref(w->id);
 		}
 
+		result.reaped_words_global += words_to_erase.size();
+
 		// here `words_to_erase' is destroyed and all word_ptr's are released, freeing memory
 
 		return result;
@@ -260,22 +291,12 @@ private:
 			curr_slice = meow::make_intrusive<wordslice_t>();
 
 		// put the word into wordslice
-		// NOTE: can't use w->hash here, since it's hashed w->str and not w->id
-		auto inserted_pair = curr_slice->ht.emplace(w->id, w);
+		auto inserted_pair = curr_slice->ht.emplace(w);
 
 		// regardless of insert success, it's fine
 		// if inserted: we're good, word is now referenced by wordslice
 		// if not:      also good, the word was already there and referenced
 		(void)inserted_pair;
-
-		return;
-
-
-		// word_ptr& ts_word = curr_slice->ht[w->id];
-		// if (!ts_word)
-		// {
-		// 	ts_word.reset(w); // increments refcount here
-		// }
 	}
 };
 
